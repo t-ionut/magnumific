79d5d15446b01f169ff9586a7b2785099a6700a2
ionut <ionut@presslabs.com>
2016-03-04 23:11:37 +0200

    make navbar text thin

--- sections/navbar.php
+++ sections/navbar.php
@@ -3,11 +3,11 @@
         <a href="<?php echo home_url(); ?>">
             <img src="/wp-content/themes/magnumific/pictures/Logo/navbar-logo.png">
         </a>
-        <a class="bold smooth" href="#">Acasă</a>
-        <a class="bold smooth" href="#despre">Despre noi</a>
-        <a class="bold smooth" href="#parteneri">Parteneri</a>
-        <a class="bold smooth" href="#echipa">Echipă</a>
-    	<a class="bold smooth" href="#cariera">Carieră</a>
-	    <a class="bold smooth" href="#contact">Contact</a>
+        <a class="smooth" href="#">Acasă</a>
+        <a class="smooth" href="#despre">Despre noi</a>
+        <a class="smooth" href="#parteneri">Parteneri</a>
+        <a class="smooth" href="#echipa">Echipă</a>
+        <a class="smooth" href="#cariera">Carieră</a>
+        <a class="smooth" href="#contact">Contact</a>
     </nav>
 </div>
