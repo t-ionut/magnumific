06d3b64ede85b34645e2ac5cc1da1982d1edca59
ionut <ionut@presslabs.com>
2016-03-04 23:39:35 +0200

    align nav items to the right

--- sections/navbar.php
+++ sections/navbar.php
@@ -1,13 +1,13 @@
 <div class="blue" id="topnav">
+    <a href="<?php echo home_url(); ?>">
+        <img src="/wp-content/themes/magnumific/pictures/Logo/navbar-logo.png">
+    </a>
     <nav>
-        <a href="<?php echo home_url(); ?>">
-            <img src="/wp-content/themes/magnumific/pictures/Logo/navbar-logo.png">
-        </a>
         <a class="smooth" href="#">Acasă</a>
         <a class="smooth" href="#despre">Despre noi</a>
         <a class="smooth" href="#parteneri">Parteneri</a>
         <a class="smooth" href="#echipa">Echipă</a>
-        <a class="smooth" href="#cariera">Carieră</a>
-        <a class="smooth" href="#contact">Contact</a>
+    	<a class="smooth" href="#cariera">Carieră</a>
+	<a class="smooth" href="#contact">Contact</a>
     </nav>
 </div>
