bd9b2dd3d0c3d3bf71b509dcb8ac70a0d52eb387
ionut <ionut@presslabs.com>
2016-03-06 14:54:56 +0200

    hide team members for now. reorganize team section. make partner logos responsive

--- sections/team-section.php
+++ sections/team-section.php
@@ -4,42 +4,34 @@
         <img style="max-width: 100%;" src="/wp-content/themes/magnumific/pictures/Poze/team-banner-1200x351.jpg">
     </div>
     <div class="team">
-        <ul>
-            <li>
-                <div class="team-icon">
-                    <img src="/wp-content/themes/magnumific/pictures/Grafic/Cravată.png">
-                </div>
-                <div class="team-icon">
-                    <img src="/wp-content/themes/magnumific/pictures/Grafic/Birou.png">
-                </div>
-                <div class="team-icon">
-                    <img src="/wp-content/themes/magnumific/pictures/Grafic/Laptop.png">
-                </div>
-            </li>
-            <li>
-                <div class="team-member">
-                    <p>Member picture here</p>
-                </div>
-                <div class="team-member">
-                    <p>Member picture here</p>
-                </div>
-                <div class="team-member">
-                    <p>Member picture here</p>
-                </div>
-            </li>
-            <li>
-                <div class="team-member">
-                    <p>Member picture here</p>
-                </div>
-                <div class="team-member">
-                    <p>Member picture here</p>
-                </div>
-                <div class="team-member">
-                    <p>Member picture here</p>
-                </div>
-            </li>
-        </ul>
-        <p class="title">Mereu de încredere!</p>
+        <div class="team-icon">
+            <img src="/wp-content/themes/magnumific/pictures/Grafic/Cravată.png">
+        </div>
+        <div class="team-icon">
+            <img src="/wp-content/themes/magnumific/pictures/Grafic/Birou.png">
+        </div>
+        <div class="team-icon">
+            <img src="/wp-content/themes/magnumific/pictures/Grafic/Laptop.png">
+        </div>
+        <div class="team-member">
+            <p>Member picture here</p>
+        </div>
+        <div class="team-member">
+            <p>Member picture here</p>
+        </div>
+        <div class="team-member">
+            <p>Member picture here</p>
+        </div>
+        <div class="team-member">
+            <p>Member picture here</p>
+        </div>
+        <div class="team-member">
+            <p>Member picture here</p>
+        </div>
+        <div class="team-member">
+            <p>Member picture here</p>
+        </div>
     </div>
+    <p class="title">Mereu de încredere!</p>
 </div>
 <div class="section-separator"></div>
