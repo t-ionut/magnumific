8b4f2caab18d11a9cfb1d3ec3b79a4f00ad9b36b
ionut <ionut@presslabs.com>
2016-03-07 04:07:32 +0200

    forgot this

---
+++ js/fliper.js
@@ -0,0 +1,52 @@
+var $ = jQuery;
+
+$(document).ready(function() {
+    $('#titam-logo img').click(function() {
+        $('#titam-desc').fadeIn();
+    });
+    $('#titam-desc').click(function() {
+        $('#titam-desc').fadeOut();
+    });
+    $('#raidex-logo img').click(function() {
+        $('#raidex-desc').fadeIn();
+    });
+    $('#raidex-desc').click(function() {
+        $('#raidex-desc').fadeOut();
+    });
+    $('#yato-logo img').click(function() {
+        $('#yato-desc').fadeIn();
+    });
+    $('#yato-desc').click(function() {
+        $('#yato-desc').fadeOut();
+    });
+    $('#pe-logo img').click(function() {
+        $('#pe-desc').fadeIn();
+    });
+    $('#pe-desc').click(function() {
+        $('#pe-desc').fadeOut();
+    });
+    $('#alligator-logo img').click(function() {
+        $('#alligator-desc').fadeIn();
+    });
+    $('#alligator-desc').click(function() {
+        $('#alligator-desc').fadeOut();
+    });
+    $('#italmatic-logo img').click(function() {
+        $('#italmatic-desc').fadeIn();
+    });
+    $('#italmatic-desc').click(function() {
+        $('#italmatic-desc').fadeOut();
+    });
+    $('#rema-logo img').click(function() {
+        $('#rema-desc').fadeIn();
+    });
+    $('#rema-desc').click(function() {
+        $('#rema-desc').fadeOut();
+    });
+    $('#hofmann-logo img').click(function() {
+        $('#hofmann-desc').fadeIn();
+    });
+    $('#hofmann-desc').click(function() {
+        $('#hofmann-desc').fadeOut();
+    });
+});
